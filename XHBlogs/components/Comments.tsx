7f6bfaeb55cd94f7c2e45b5bc33e08ff89cbaa20
猫小鱼 <122292704+hyhhyh123@users.noreply.github.com>
2026-08-25 15:53:01 +0800

    Update Comments.tsx

--- XHBlogs/components/Comments.tsx
+++ XHBlogs/components/Comments.tsx
@@ -26,7 +26,7 @@ export default function Comments() {
           data-reactions-enabled="1"
           data-emit-metadata="0"
           data-input-position="bottom"
-          data-theme="https://xhblogs-hyh10.vercel.app/giscus-theme.css"
+          data-theme="https://xhblogs-hyh10.vercel.app/giscus-theme.css?v=2"
           data-lang="zh-CN"
           data-loading="lazy"
           strategy="afterInteractive"
