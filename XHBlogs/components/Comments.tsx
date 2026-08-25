d2f78f5be342b2cdc932be9cb0e3e76e0ca664ef
猫小鱼 <122292704+hyhhyh123@users.noreply.github.com>
2026-08-25 16:14:47 +0800

    Update Comments.tsx: use built-in light_high_contrast theme

--- XHBlogs/components/Comments.tsx
+++ XHBlogs/components/Comments.tsx
@@ -26,7 +26,7 @@ export default function Comments() {
           data-reactions-enabled="1"
           data-emit-metadata="0"
           data-input-position="bottom"
-          data-theme="https://xhblogs-hyh10.vercel.app/giscus-theme.css?v=3"
+          data-theme="light_high_contrast"
           data-lang="zh-CN"
           data-loading="lazy"
           strategy="afterInteractive"
